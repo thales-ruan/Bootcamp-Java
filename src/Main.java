8091f57771a8fc498b5c4ab26d6cf5f176d818e0
Thales Ruan <thalesruandamassacla@gmail.com>
2023-03-07 22:51:08 -0300

    terminal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("funcionando no terminal");
     }
 }
 //ESCOPO
 //Variavel vai de arcodo com o bloco que ela foi declarada
+
+
+//Terminal
+//java MinhaClass obs: na pasta out do projeto
