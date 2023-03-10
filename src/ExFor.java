20920bac3e719624c740636e597fc0607a131430
Thales Ruan <thalesruandamassacla@gmail.com>
2023-03-09 21:41:13 -0300

    ex01

---
+++ src/ExFor.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ExFor {
+
+    //todo Classe para teste
+
+
+    public static void main(String[] args) {
+//        String nome;
+//        int idade;
+//        Scanner scan = new Scanner(System.in);
+//
+//        do {
+//            System.out.println("Digite 0 para sair ou 1 para continuar:");
+//            String resposta = scan.next();
+//
+//            if (resposta.equals("0")) {
+//                break;
+//            } else if (resposta.equals("1")) {
+//                System.out.println("Nome: ");
+//                nome = scan.next();
+//
+//                System.out.println("Idade: ");
+//                while (!scan.hasNextInt()){
+//                    System.out.println("Idade invalida digite apenas numeros! ");
+//                    scan.next();
+//                }
+//
+//
+//                idade = scan.nextInt();
+//
+//                System.out.println("Seu nome é: " + nome + " e sua idade é: " + idade);
+//            }else {
+//                break;
+//            }
+//        } while (true);
+    }
+
+}
